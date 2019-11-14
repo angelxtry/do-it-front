2176a836f0eb9b6403048ef6de90a6ad60b93bf0
LeeSuHo <angelxtry@gmail.com>
2019-11-14 19:26:37 +0900

    Set url and script for deploy

--- sagas/index.js
+++ sagas/index.js
@@ -5,9 +5,10 @@ import todoHistory from './todoHistory';
 import user from './user';
 import timer from './timer';
 
-// axios.defaults.baseURL = `http://15.164.163.120:8085/api`;
-// axios.defaults.baseURL = `http://api.mygraphr.com:8085/api`;
-axios.defaults.baseURL = `http://localhost:8085/api`;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'http://api.doitreviews.com:8085/api'
+    : `http://localhost:8085/api`;
 
 function* rootSaga() {
   yield all([call(todoHistory), call(user), call(timer)]);
